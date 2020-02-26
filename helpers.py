4241a48d69f394584f7c3c4be4f83eb040312c6b
Jakkarin Sae-Tiew <jakkarin.sae-tiew@outlook.com>
2020-02-26 14:52:48 +0800

    Restructure into modules

--- helpers.py
+++ helpers.py
@@ -1,16 +1,19 @@
 from matplotlib import pyplot as plt
+from matplotlib.colors import hsv_to_rgb
+import cv2
 
 # Some helper functions
-def show1img(img, img_title):
+def show1img(img_title, img):
+#     img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
     figure_size = 10
     plt.figure(figsize=(figure_size, figure_size))
     plt.imshow(img)
     plt.title(img_title)
-
     plt.show()
 
-
-def show2img(img1, img1_title, img2, img2_title):
+def show2img(img1_title, img1,img2_title, img2):
+#     img1 =  cv2.cvtColor(img1, cv2.COLOR_HSV2RGB)
+#     img2 =  cv2.cvtColor(img2, cv2.COLOR_HSV2RGB)
     figure_size = 20
     plt.figure(figsize=(figure_size, figure_size))
     plt.subplot(1, 2, 1), plt.imshow(img1)
